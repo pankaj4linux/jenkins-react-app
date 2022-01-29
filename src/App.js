56dc4c88f4f7f1b841b6d2338c16e17377692f6a
pankaj4linux <60768578+pankaj4linux@users.noreply.github.com>
2022-01-29 18:04:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. pankaj kumar updated
+          Edit <code>src/App.js</code> and save to reload. pankaj kumar new updated
         </p>
         <a
           className="App-link"
